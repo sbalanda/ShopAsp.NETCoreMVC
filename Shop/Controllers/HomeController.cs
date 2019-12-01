08d2f0a657984c8adb30135b6d4a1f349ca4a098
Sergey Balanda <s.b_y@mail.ru>
2019-12-01 14:19:54 +0300

    #8 - Главная страница сайта

---
+++ Shop/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+using Shop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAllCars _carRep;
+
+        public HomeController(IAllCars carRep)
+        {
+            _carRep = carRep;
+        }
+
+        public ViewResult Index()
+        {
+            var homeCars = new HomeViewModel
+            {
+                FavCars = _carRep.GetFavCars
+            };
+            return View(homeCars);
+        }
+    }
+}
